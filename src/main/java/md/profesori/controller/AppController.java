1cc98f786b0fae35d6f46736df673c6898ed06a7
mark <mark.graciov@gmail.com>
2016-04-05 01:03:32 +0300

    Configured under boot and thymeleaf project

--- src/main/java/md/profesori/controller/AppController.java
+++ src/main/java/md/profesori/controller/AppController.java
@@ -1,10 +1,8 @@
 package md.profesori.controller;
 
-import md.profesori.entities.Professor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -14,14 +12,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class AppController {
 
     @RequestMapping("/")
-    public String home() {
+    public String index() {
         return "index";
     }
 
-    @RequestMapping("/{category}")
-    public String categoryView(@PathVariable("category") String category) {
-        Professor p = new Professor();
-
-        return "categoryView";
+    @RequestMapping("/{link}")
+    @ResponseBody
+    public String compartment(@PathVariable("link") String link) {
+        return "Ati accesat linkul " + link;
     }
+
 }
